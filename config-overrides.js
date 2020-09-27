5456829d083d9f166ffbe7fbae4a7e859dafbc82
chihokyo <chihoukyo@gmail.com>
2020-09-27 23:59:57 +0900

    【Step1】实现antd自定义主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,18 @@
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 
 module.exports = override(
     // 针对antd实现按需打包，根据import来打包（babel-plugin-import）
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: 'css',// 自动打包相关样式
+        style: true,// 自动打包相关样式
+    }),
+    addLessLoader({
+        lessOptions: {
+            javascriptEnabled: true,
+            // @primary-color → 变量名 所以这个就是修改样式变量。
+            // 本质是修改了less文件中的源码
+            modifyVars: { '@primary-color': '#1DA57A' },
+        },
     }),
 )
