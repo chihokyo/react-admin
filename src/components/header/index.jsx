aa1baa13b6da3da02a34b70e54b568d89f5f3e12
chihokyo <chihoukyo@gmail.com>
2020-10-02 00:18:17 +0900

    【Step1】登录页面-动态获取title

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import { reqWeather } from '../../api/index'
+import menuList from '../../config/menuConfig'
 import { formateDate } from '../../utils/dateUtils'
 import memoryUtils from '../../utils/memoryUtils'
 
@@ -7,13 +9,34 @@ import './index.less'
 /**
  * 左侧导航组件
  */
-export default class Header extends Component {
+class Header extends Component {
 
     // 状态
     state = {
         currentTime: formateDate(Date.now()), // 当前时间字符串
         dayPictureUrl: '', // 天气图片url
         weather: '', // 天气文本
+    }
+
+    // 获取body相应的title
+    getTitle = () => {
+        const path = this.props.location.pathname
+        let title
+        // 如果当前item对应的key和目前path的请求地址一样
+        // item.title就是当前的title
+        menuList.forEach(item => {
+            if (item.key === path) {
+                title = item.title
+                // 在左右的子item里面进行再次查找匹配
+            } else if (item.children) {
+                const CItem = item.children.find(cItem => cItem.key === path)
+                // 如果有值，证明匹配成功
+                if (CItem) {
+                    title = CItem.title
+                }
+            }
+        })
+        return title
     }
 
     // 每隔1s获取时间
@@ -48,7 +71,7 @@ export default class Header extends Component {
     render() {
         const { currentTime, dayPictureUrl, weather } = this.state
         const username = memoryUtils.user.username
-
+        const title = this.getTitle()
         return (
             <div className="header">
                 <div className="header-top">
@@ -56,7 +79,7 @@ export default class Header extends Component {
                     <a href="javascript;">退出</a>
                 </div>
                 <div className="header-bottom">
-                    <div className="header-bottom-left">首页</div>
+                    <div className="header-bottom-left">{title}</div>
                     <div className="header-bottom-right">
                         <span>{currentTime}</span>
                         <img src={dayPictureUrl} alt="weather" />
@@ -67,3 +90,6 @@ export default class Header extends Component {
         )
     }
 }
+
+
+export default withRouter(Header)
