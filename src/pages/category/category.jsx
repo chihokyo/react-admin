e22cd014e781cfb0f33ea4999a7b22d1acd87710
chihokyo <chihoukyo@gmail.com>
2020-10-02 15:43:39 +0900

    【Step1】内容页面-显示2级列表&退回1级列表

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -4,8 +4,9 @@ import {
   Button,
   Table, message
 } from 'antd'
+import { ArrowRightOutlined } from '@ant-design/icons'
 import LinkButton from '../../components/link-button'
-import { reqCategorys, reqAddCategory, reqUpdateCategory } from '../../api/'
+import { reqCategorys } from '../../api/'
 /**
  * 商品分类路由
  */
@@ -13,7 +14,10 @@ export default class Category extends Component {
 
   state = {
     loading: false, // 是否正在获取数据
-    categorys: [], // 1级分类列表
+    category: [], // 1级分类列表
+    parentId: '0', // 当前显示的父类列表ID
+    parentName: '', // 当前显示的父类列表title
+    subCategorys: []
   }
 
   /**
@@ -30,10 +34,14 @@ export default class Category extends Component {
         width: 300,
         dataIndex: 'age',
         // 指定需要返回的页面标签
-        render: () => (
+        render: (category) => (
           <span>
             <LinkButton>修改分类</LinkButton>
-            <LinkButton>查看子分类</LinkButton>
+            {/* 下面是错误的，因为初始化的时候不能调用onclick，这样就会直接执行了
+            而需要在触发事件进行调用的话。外面就需要在进行包裹一层函数 */}
+            {/* <LinkButton onClick={ this.shouldComponentUpdate(category)}>查看子分类</LinkButton> */}
+            {/* 如何向事件回调函数进行传递参数，先定义一个回调函数，里面在定义一个函数传入数据 */}
+            {this.state.parentId === '0' ? <LinkButton onClick={() => { this.shouldComponentUpdate(category) }}>查看子分类</LinkButton> : null}
           </span>
         )
       }
@@ -41,21 +49,69 @@ export default class Category extends Component {
   }
 
   /**
-   * 获取1级分类列表
+   * 获取1级or2级分类列表
    */
   getCategorys = async () => {
     // 请求前loading
     this.setState({ loading: true })
-    const result = await reqCategorys('0')
+    // 获取此时列表id
+    const { parentId } = this.state
+    // 获取1级or2级列表数据
+    const result = await reqCategorys(parentId)
+
     this.setState({ loading: false })
+
+    // 判断请求是否成功
+    // 成功
     if (result.status === '0') {
-      const categorys = result.data
-      this.setState({
-        categorys
-      })
+      const category = result.data
+      // 判断结果是1级列表or2级列表
+      // 1级
+      if (parentId === '0') {
+        this.setState({
+          category
+        })
+        // 2级
+      } else {
+        this.setState({
+          subCategorys: category
+        })
+      }
+      // 失败
     } else {
       message.error('获取分类列表失败')
     }
+  }
+
+  /**
+   * 获取指定1级列表的孩子们2级子列表
+   * @param {string} category 
+   */
+  showSubCategorys = (category) => {
+    // 先更新 parentId 状态
+    this.setState({
+      parentId: category._id,
+      parentName: category.name
+    }, () => {
+      // console.log(this.state.parentId)
+      // 这里必须要写在闭包函数里！！！
+      // 因为setState()不能立即获取最新的状态，他是异步更新状态
+      // 获取2级分类
+      this.getCategorys()
+    })
+    // console.log(this.state.parentId)
+  }
+
+  /**
+   * 显示1级分类列表
+   */
+  showMainCategorys = () => {
+    // 本质就是更新为显示1级列表的状态
+    this.setState({
+      parentId: '0',
+      parentName: '',
+      subCategorys: []
+    })
   }
 
   // 初始化列表数据
@@ -70,9 +126,16 @@ export default class Category extends Component {
   }
 
   render() {
-    const { categorys, loading } = this.state
+    const { category, loading, subCategorys, parentId, parentName } = this.state
     // card标题
-    const title = '一级分类'
+    const title = parentId === '0' ? '1级分类列表' : (
+      <span>
+        <LinkButton onClick={this.showMainCategorys}>1级分类列表</LinkButton>
+        <ArrowRightOutlined />
+        <span style={{ marginLeft: '10px' }} >{parentName}</span>
+      </span>
+    )
+
     const extra = (
       <Button type="primary">
         添加
@@ -86,7 +149,7 @@ export default class Category extends Component {
             bordered={true}
             rowKey='_id'
             loading={loading}
-            dataSource={categorys}
+            dataSource={parentId === '0' ? category : subCategorys}
             columns={this.columns}
             pagination={{ defaultPageSize: 10, showQuickJumper: true }}
           />
